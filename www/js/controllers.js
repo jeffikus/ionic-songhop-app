1a0463c190e07ea4fd466ab23bf2c92d8f390285
Eric Simons <ericsimons@es40.net>
2015-02-02 17:27:22 -0800

    ch1 final

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,57 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope) {
+.controller('DiscoverCtrl', function($scope, $timeout, User) {
+  // our first three songs
+  $scope.songs = [
+     {
+        "title":"Stealing Cinderella",
+        "artist":"Chuck Wicks",
+        "image_small":"https://i.scdn.co/image/d1f58701179fe768cff26a77a46c56f291343d68",
+        "image_large":"https://i.scdn.co/image/9ce5ea93acd3048312978d1eb5f6d297ff93375d"
+     },
+     {
+        "title":"Venom - Original Mix",
+        "artist":"Ziggy",
+        "image_small":"https://i.scdn.co/image/1a4ba26961c4606c316e10d5d3d20b736e3e7d27",
+        "image_large":"https://i.scdn.co/image/91a396948e8fc2cf170c781c93dd08b866812f3a"
+     },
+     {
+        "title":"Do It",
+        "artist":"Rootkit",
+        "image_small":"https://i.scdn.co/image/398df9a33a6019c0e95e3be05fbaf19be0e91138",
+        "image_large":"https://i.scdn.co/image/4e47ee3f6214fabbbed2092a21e62ee2a830058a"
+     }
+  ];
+
+  // initialize the current song
+  $scope.currentSong = angular.copy($scope.songs[0]);
+
+
+  // fired when we favorite / skip a song.
+  $scope.sendFeedback = function (bool) {
+
+    // first, add to favorites if they favorited
+    if (bool) User.addSongToFavorites($scope.currentSong);
+
+    // set variable for the correct animation sequence
+    $scope.currentSong.rated = bool;
+    $scope.currentSong.hide = true;
+
+    $timeout(function() {
+      // $timeout to allow animation to complete
+      // set the current song to one of our three songs
+      var randomSong = Math.round(Math.random() * ($scope.songs.length - 1));
+
+      // update current song in scope
+      $scope.currentSong = angular.copy($scope.songs[randomSong]);
+
+    }, 250);
+
+  }
+
+
+
 
 })
 
@@ -12,14 +62,19 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope) {
+.controller('FavoritesCtrl', function($scope, User) {
+  // get the list of our favorites from the user service
+  $scope.favorites = User.favorites;
 
+  $scope.removeSong = function(song, index) {
+    User.removeSongFromFavorites(song, index);
+  }
 })
 
 
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, User) {
 
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,27 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', [])
+
+.factory('User', function() {
+  
+  var o = {
+    favorites: []
+  }
+
+  o.addSongToFavorites = function(song) {
+    // make sure there's a song to add
+    if (!song) return false;
+
+    // add to favorites array
+    o.favorites.unshift(song);
+    o.newFavorites++;
+  }
+
+  o.removeSongFromFavorites = function(song, index) {
+    // make sure there's a song to add
+    if (!song) return false;
+
+    // add to favorites array
+    o.favorites.splice(index, 1);
+  }
+
+  return o;
+});
