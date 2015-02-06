7c34891fc1f2d9ca98178a33f9e20991894ac81a
Eric Simons <ericsimons@es40.net>
2015-02-05 19:59:59 -0800

    partial

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,36 @@
 angular.module('songhop.services', [])
 
+.factory('Recommendations', function($q, $http, SERVER) {
+  
+  var o = {
+    queue: []
+  };
+
+  o.getNextSongs = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/recommendations'
+    }).success(function(data){
+      // merge data into the queue
+      o.queue = o.queue.concat(data);
+    });
+  }
+
+
+  o.nextSong = function() {
+    // pop the index 0 off
+    o.queue.shift();
+  }
+
+  // low on the queue? lets fill it up
+  if (o.queue.length <= 3) {
+    o.getNextSongs();
+  }
+
+  return o;
+})
+
+
 .factory('User', function() {
   
   var o = {
